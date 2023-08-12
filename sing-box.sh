01e4a9e513ef0c362d86b455ee2b0dd62601a72a
yagmi14 <63093840+yagmi14@users.noreply.github.com>
2023-08-12 18:46:10 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -5,8 +5,9 @@ read -p "请输入域名：" domain
 echo "请选择:"
 echo "1) vless-xtls-grpc-reality"
 echo "2) vless-xtls-vision-reality"
+echo "3) shadowsocks"
 
-read -p "输入(1-2):" choice
+read -p "输入(1-3):" choice
 
 case $choice in
   1)
@@ -19,6 +20,9 @@ case $choice in
     echo '{"log":{"level":"info","timestamp":true},"route":{"rules":[{"geosite":"cn","geoip":"cn","outbound":"direct"},{"geosite":"category-ads-all","outbound":"block"}]},"inbounds":[{"type":"vless","tag":"vless-in","listen":"::","listen_port":443,"sniff":true,"sniff_override_destination":true,"users":[{"uuid":"f8b5cc81-d25c-4d22-92b6-d10a055f7e98","flow":"xtls-rprx-vision"}],"tls":{"enabled":true,"server_name":"'"$domain"'","reality":{"enabled":true,"handshake":{"server":"'"$domain"'","server_port":443},"private_key":"oOyJjI_Cdn5CfDoKK9HtLai8HVS0jfBbHUz3ytRhOUY","short_id":["4c10a4acb2917613"]}}}], "outbounds":[{"type":"direct","tag":"direct"},{"type":"block","tag":"block"}]}' > /usr/local/etc/sing-box/config.json; \
     /usr/local/bin/sing-box run -c /usr/local/etc/sing-box/config.json
     ;;
+  3)
+    echo "shadowsocks"
+    
   *)
     echo "无效输入,请重试!"
     ;;
