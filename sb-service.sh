06d54c54b63208c41de2687f474e663b3018a74c
yagmi14 <63093840+yagmi14@users.noreply.github.com>
2023-08-14 17:18:17 +0800

    Create sb-service.sh

---
+++ sb-service.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+read -p "name:" name
+
+cat > /etc/systemd/system/$name.service <<EOF
+[Unit]
+Description=sing-box service
+Documentation=https://sing-box.sagernet.org
+After=network.target nss-lookup.target
+
+[Service]
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+ExecStart=/usr/local/bin/sing-box run -c /usr/local/etc/$name/config.json
+Restart=on-failure
+RestartSec=1800s
+TasksMax=infinity
+LimitCPU=infinity
+LimitFSIZE=infinity
+LimitDATA=infinity
+LimitSTACK=infinity
+LimitCORE=infinity
+LimitRSS=infinity
+LimitNOFILE=infinity
+LimitAS=infinity
+LimitNPROC=infinity
+LimitMEMLOCKS=infinity
+LimitSIGPENDING=infinity
+LimitMSGQUEUE=infinity
+LimitRPTRIO=infinity
+LimitRTTIME=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable --now $name; \
+systemctl status $name
