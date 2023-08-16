358ecaecd1fde8640efee21aed333887eddd141d
yagmi14 <63093840+yagmi14@users.noreply.github.com>
2023-08-16 13:31:26 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -3,6 +3,8 @@
 echo "请选择:"
 echo "1) 🇭🇰 IPRaft-HKG1"
 echo "2) 🇺🇦 GlobalVM-IEV"
+echo "3) 🇮🇸 1984-REF"
+echo "4) 🇮🇱 Stark-IL"
 
 read -p "输入选择:" choice
 
@@ -14,6 +16,14 @@ case $choice in
   2)
     echo "🇺🇦 GlobalVM-IEV"
     ssh root@2a13:b487:4f00::50
+    ;;
+  3)
+    echo "🇮🇸 1984-REF"
+    ssh root@is.1984.wx2021.buzz -p 20031
+    ;;
+  4)
+    echo "🇮🇱 Stark-IL"
+    ssh root@il.sisl.wx2021.buzz -p 10016
     ;;
   *)
     echo "无效输入,请重试!"
