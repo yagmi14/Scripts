bed9c850c1224e8e822aa9f7e164cb5cfefa64e2
yagmi14 <63093840+yagmi14@users.noreply.github.com>
2023-08-18 21:55:40 +0800

    Create create_file.sh

---
+++ create_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 获取用户输入的文件名
+echo "请输入文件名："
+read filename
+
+# 创建一个新文件
+touch "$filename"
+
+# 打开文件并将用户输入的内容写入其中
+echo "请输入文本内容："
+while read line; do
+  echo "$line" >> "$filename"
+done
+
+# 给予文件权限
+chmod +x "$filename"
+
+# 提示用户完成
+echo "文件已创建并写入。"
