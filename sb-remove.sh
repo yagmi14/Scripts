4a2f6dad13c807cfe879d7b7c88c7a0cb01bb091
yagmi14 <63093840+yagmi14@users.noreply.github.com>
2023-08-22 16:50:55 +0800

    Update sb-remove.sh

--- sb-remove.sh
+++ sb-remove.sh
@@ -2,4 +2,4 @@
 
 read -p "name:" name
 
-systemctl disable --now $name; rm -f /etc/systemd/system/$name.service; rm -rf /usr/local/etc/$name
+systemctl disable --now $([ -f /etc/systemd/system/$name.service ] && echo $name); rm -f /etc/systemd/system/$name.service; rm -rf /usr/local/etc/$name
