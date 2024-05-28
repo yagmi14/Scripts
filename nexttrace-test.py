031e3f416a4a96994cbc7a13c528f28baa4ec725
yagmi14 <63093840+yagmi14@users.noreply.github.com>
2024-05-28 17:05:23 +0800

    Update nexttrace-test.py

--- nexttrace-test.py
+++ nexttrace-test.py
@@ -163,7 +163,7 @@ try:
             subprocess.call(["nexttrace", "-MT4", "sgp.proof.ovh.net"]) # OVH-SIN
             subprocess.call(["nexttrace", "-MT4", "sg.lg.speedypage.com"]) # SpeedyPage-SIN            
             subprocess.call(["nexttrace", "-MT4", "sgp-ping.vultr.com"]) # Vultr-SIN
-               subprocess.call(["nexttrace", "-MT", "185.194.54.1"]) # xTom-SIN         
+            subprocess.call(["nexttrace", "-MT", "185.194.54.1"]) # xTom-SIN         
             subprocess.call(["nexttrace", "-MT", "54.254.128.1"]) # AWS-SIN
             subprocess.call(["nexttrace", "-MT", "s3southeastasia.blob.core.windows.net"]) # Azure-SIN
             # Add more tracing commands for SG option as needed
