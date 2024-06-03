8227dc6e6b32d0b0864d39e85b4a3f82fad487f4
yagmi14 <63093840+yagmi14@users.noreply.github.com>
2024-06-03 23:34:43 +0800

    Update sb2_conf.sh

--- sb2_conf.sh
+++ sb2_conf.sh
@@ -3,16 +3,16 @@ set -e
 
 bash <(curl -fsSL https://sing-box.app/deb-install.sh)
 
-mkdir -p /usr/local/etc/sing-box/conf
+mkdir -p /usr/local/etc/sb2/conf
 
-echo '{"log":{"disabled":false,"level":"info","timestamp":true}}' > /usr/local/etc/sing-box/conf/00_log.json
+echo '{"log":{"disabled":false,"level":"info","timestamp":true}}' > /usr/local/etc/sb2/conf/00_log.json
 
-echo '{"route":{"rule_set":[{"tag":"geosite-openai","type":"remote","format":"binary","url":"https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-openai.srs"}],"rules":[{"domain":"api.openai.com","outbound":"direct"},{"rule_set":"geosite-openai","outbound":"direct"}]}}' > /usr/local/etc/sing-box/conf/02_route.json
+echo '{"route":{"rule_set":[{"tag":"geosite-openai","type":"remote","format":"binary","url":"https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-openai.srs"}],"rules":[{"domain":"api.openai.com","outbound":"direct"},{"rule_set":"geosite-openai","outbound":"direct"}]}}' > /usr/local/etc/sb2/conf/02_route.json
 
-echo '{"experimental":{"cache_file":{"enabled":true,"path":"/usr/local/etc/sing-box/cache.db"}}}' > /usr/local/etc/sing-box/conf/03_experimental.json
+echo '{"experimental":{"cache_file":{"enabled":true,"path":"/usr/local/etc/sb2/cache.db"}}}' > /usr/local/etc/sb2/conf/03_experimental.json
 
-echo '{"dns":{"servers":[{"address":"local"}]}}' > /usr/local/etc/sing-box/conf/04_dns.json
+echo '{"dns":{"servers":[{"address":"local"}]}}' > /usr/local/etc/sb2/conf/04_dns.json
 
-echo -e "[Unit]\nDescription=sing-box service\nDocumentation=https://sing-box.sagernet.org\nAfter=network.target nss-lookup.target\n\n[Service]\nUser=root\nType=simple\nNoNewPrivileges=yes\nTimeoutStartSec=0\nExecStart=/usr/bin/sing-box run -C /usr/local/etc/sing-box/conf/\nExecReload=/bin/kill -HUP \$MAINPID\nRestart=on-failure\nRestartSec=10\nLimitNOFILE=infinity\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/sb3.service
+echo -e "[Unit]\nDescription=sing-box service\nDocumentation=https://sing-box.sagernet.org\nAfter=network.target nss-lookup.target\n\n[Service]\nUser=root\nType=simple\nNoNewPrivileges=yes\nTimeoutStartSec=0\nExecStart=/usr/bin/sing-box run -C /usr/local/etc/sb2/conf/\nExecReload=/bin/kill -HUP \$MAINPID\nRestart=on-failure\nRestartSec=10\nLimitNOFILE=infinity\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/sb2.service
 
-sudo systemctl enable sb3
+sudo systemctl enable sb2
